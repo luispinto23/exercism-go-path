e6ee71dd5fe5c82c04ab923e77e94062e6e46908
Luís Pinto <luispinto23@gmail.com>
2021-08-13 23:02:33 +0100

    Initial commit

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// This package implements a simple function that greets the user with the message "Hello, World!"
+package greeting
+
+// This function prints the message "Hello, World!" to the console
+func HelloWorld() string {
+	return "Hello, World!"
+}
